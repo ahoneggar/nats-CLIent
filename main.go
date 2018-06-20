673cc218cf6230432d94b9775b6d44a087a68d6b
ahoneggar <35458190+ahoneggar@users.noreply.github.com>
2018-06-20 10:59:21 -0400

    non-tls full rough draft

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"github.com/nats-io/go-nats"
+	"strings"
+	"time"
 )
 
 type options struct {
@@ -14,6 +16,24 @@ type options struct {
 	key string
 	ca string
 }
+
+const (
+	welcome = `
+#####################################################################
+#                      WELCOME TO NATS-CLIent!                      #
+#-------------------------------------------------------------------#
+#    PUBLISH   - PUB <subject> <message>                            #
+#    SUBSCRIBE - SUB <subject>                                      #
+#    REQUEST   - REQ <subject> <message>                            #
+#    HELP      - H | HELP (prints this message again)               #
+#####################################################################
+`
+	subUsage = "Sub usage: SUB <subject>\n"
+	pubUsage = "Pub usage: PUB <subject> <message>\n"
+	reqUsage = "Req usage: REQ <subject> <message>\n"
+)
+
+var nc nats.Conn
 
 func main() {
 	opts := parseOpts()
@@ -39,24 +59,94 @@ func parseOpts() *options {
 }
 
 func testConn(opts *options) {
-	if !opts.tls {
-		nc, err := nats.Connect(opts.host)
-		if err != nil {
-			fmt.Errorf("Connection Failed: %+v", err)
-			return
-		}
-		defer nc.Close()
-		if nc.IsConnected() {
-			fmt.Println("Connection Successful")
-			return
-		}
+	if opts.tls {
+		//testConnWithTLS(opts)
+	}
+	nc, err := nats.Connect(opts.host)
+	if err != nil {
 		fmt.Errorf("Connection Failed: %+v", err)
 		return
 	}
+	defer nc.Close()
+	if nc.IsConnected() {
+		fmt.Println("Connection Successful")
+		return
+	}
+	fmt.Errorf("Connection Failed: %+v", err)
+	return
 
 	// TODO: TLS connection test
 }
 
 func fullClient(opts *options) {
 	// TODO: CLI client similar to telnet that parses sub/pub commands
+	if opts.tls {
+		//fullClientWithTLS(opts)
+	}
+	fmt.Printf("Connecting to %s\n", opts.host)
+	nc, err := nats.Connect(opts.host)
+	if err != nil {
+		fmt.Errorf("Connection Failed: %+v", err)
+		return
+	}
+	defer nc.Close()
+
+	fmt.Print(welcome)
+	for true {
+		line := ""
+		fmt.Scanln(&line)
+		input := strings.Split(line, " ")
+		switch strings.ToUpper(input[0]) {
+		case "P", "PUB", "PUBLISH":
+			go publish(input)
+		case "S", "SUB", "SUBSCRIBE":
+			go subscribe(input)
+		case "R", "REQ", "REQUEST":
+			go request(input)
+		case "H", "HELP":
+			fmt.Print(welcome)
+		default:
+			fmt.Println("Unrecognized command")
+		}
+	}
+}
+
+func publish(input []string) {
+	if len(input) < 3 {
+		fmt.Print(pubUsage)
+		return
+	}
+	err := nc.Publish(input[1], []byte(strings.Join(input[2:], " ")))
+	if err != nil {
+		fmt.Printf("Error Publishing: %+v\n", err)
+	}
+}
+
+func subscribe(input []string) {
+	if len(input) < 2 {
+		fmt.Print(subUsage)
+		return
+	}
+	_, err := nc.Subscribe(input[1], handleIncomingMessage)
+	if err != nil {
+		fmt.Printf("Error Subscribing: %+v\n", err)
+		return
+	}
+}
+
+func request(input []string) {
+	if len(input) < 3 {
+		fmt.Print(reqUsage)
+		return
+	}
+	msg, err := nc.Request(input[1], []byte(strings.Join(input[2:], " ")), 10*time.Millisecond)
+	if err != nil {
+		fmt.Printf("Error Requesting: %+v\n", err)
+		return
+	}
+	fmt.Printf("Response: %s", string(msg.Data))
+}
+
+func handleIncomingMessage(m *nats.Msg) {
+	fmt.Printf("+MSG %s: %s\n", m.Subject, string(m.Data))
 }
